cf35e17b5d02b5a5dcd336d5080e88e22628e94c
Vishesh-Jangid <visheshjangid837@gmail.com>
2026-07-31 21:43:44 +0530

    This is i Copied from the duck.ai

---
+++ Chapter2/Checking_Armstrong_no.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, rem, sum = 0, digits = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        n /= 10;
+        digits++;
+    }
+
+    n = original;
+
+    while (n != 0) {
+        rem = n % 10;
+        int power = 1;
+
+        for (int i = 0; i < digits; i++) {
+            power *= rem;
+        }
+
+        sum += power;
+        n /= 10;
+    }
+
+    if (sum == original)
+        printf("%d is an Armstrong number\n", original);
+    else
+        printf("%d is not an Armstrong number\n", original);
+
+    return 0;
+}
---
+++ Chapter2/ap_sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int  a=5,b=8,c=889;
+    sum=a+b+c;
+    muliply=a*b*c;
+    d=a/b;
+    sub=-(a-b-c);
+    printf("%d\n   %d\n   %d\n %d\n",sum,muliply,d,sub);//thanks for watching this code this is simplest code today only i am not learning coding today i learning how to push the codes in the github using vs code 
+    printf("My name is Vishesh Jangid");
+}
