8a07b6459121e4b038089e5a93d11bc037b181f4
Vishesh-Jangid <visheshjangid837@gmail.com>
2026-07-31 17:24:48 +0530

    Fixed print program

---
+++ chapter1/calculator.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the Numbers"))
+b=int(input("Enter value of b"))
+##to make the calculator i used here these 
+m=a*b
+d=a/b
+add=a+b
+minus=a-b
+print("The Addition of the Numbers : ",add)
+print("The Subtration of the NUmbers : ",minus)
+print("The Multiplication of the Numbers : ",m)
+print("The Division of the NUmbers : ",d)
--- chapter1/converting_a_string_in_int_format.py
+++ chapter1/converting_a_string_in_int_format.py
@@ -1,5 +1,6 @@
 ##like if we take this thing then this will come 
-i=9
+i=1
 n=10008
-for i in n:
-    print("My name is Vishesh Jangid Baruat Wale ")
+for i in range(10009):
+    if i!=0:
+        print("My name is Vishesh Jangid Baruat Wale ")
