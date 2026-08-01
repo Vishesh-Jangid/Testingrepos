16cdba430505032b02aea49833d4e8cd1c8b7534
Vishesh-Jangid <visheshjangid837@gmail.com>
2026-08-01 09:50:27 +0530

    this is my First Function Which is Completely Made by me only Thanks for Watching this Again Thanks !! 💖😂😊😊💖❤️😍😍👍👍✌️✌️🤷‍♂️🤷‍♀️|

---
+++ Chapter-3/pyothon.py
@@ -0,0 +1,7 @@
+def name():
+    i=0
+    for i in range(1009):
+        if i!=0:
+            print("Hi , Vishesh Jangid Ji how are you ",i)
+        print("Vishesh Jangid JI is Brother of Kunjan Jangid",i)
+name()
